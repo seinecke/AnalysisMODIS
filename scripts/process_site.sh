5ddbc33985c354fc5c81e5449c0978bde27a84d6
Sabrina Einecke <sabrina.einecke@uni-dortmund.de>
2020-04-06 22:23:46 +0930

    Now for MOD and MYD processing

--- scripts/process_site.sh
+++ scripts/process_site.sh
@@ -4,8 +4,8 @@ source activate modis
 
 mkdir $1
 
-python scripts/ProcessImages.py "MYD06_L2/2015/*/*.hdf" -l $1 -f $1/ -o 2015.csv --maps
-python scripts/ProcessImages.py "MYD06_L2/2016/*/*.hdf" -l $1 -f $1/ -o 2016.csv --maps
-python scripts/ProcessImages.py "MYD06_L2/2017/*/*.hdf" -l $1 -f $1/ -o 2017.csv --maps
-python scripts/ProcessImages.py "MYD06_L2/2018/*/*.hdf" -l $1 -f $1/ -o 2018.csv --maps
-python scripts/ProcessImages.py "MYD06_L2/2019/*/*.hdf" -l $1 -f $1/ -o 2019.csv --maps
+python scripts/ProcessImages.py "M*D06_L2/2015/*/*.hdf" -l $1 -f $1/ -o 2015.csv --maps
+python scripts/ProcessImages.py "M*D06_L2/2016/*/*.hdf" -l $1 -f $1/ -o 2016.csv --maps
+python scripts/ProcessImages.py "M*D06_L2/2017/*/*.hdf" -l $1 -f $1/ -o 2017.csv --maps
+python scripts/ProcessImages.py "M*D06_L2/2018/*/*.hdf" -l $1 -f $1/ -o 2018.csv --maps
+python scripts/ProcessImages.py "M*D06_L2/2019/*/*.hdf" -l $1 -f $1/ -o 2019.csv --maps
