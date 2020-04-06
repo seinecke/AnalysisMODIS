bcbbd3b43b7c950825d690526ccb1d92d75bbbf5
Sabrina Einecke <sabrina.einecke@uni-dortmund.de>
2020-04-06 22:08:47 +0930

    Re-structure of processing and new attributes. Also new processing scripts for every site.

--- scripts/ProcessImages.py
+++ scripts/ProcessImages.py
@@ -7,7 +7,7 @@ import glob
 from geotiepoints.geointerpolator import GeoInterpolator
 import matplotlib.pyplot as plt
 import re
-import datetime
+from datetime import datetime, timedelta
 import calendar
 import click
 import matplotlib.patches as patches
@@ -17,30 +17,50 @@ import matplotlib.patches as patches
 @click.option('-f', '--output_folder', 
 			default='./')
 @click.option('-o', '--output_file')
-@click.option('-s', '--size_map',
-				default=0.25)
-@click.option('--size_mean', 
-			default=0.25)
 @click.option('-c', '--coords',
 			nargs=2, type=float,
 			help = 'Latitude of desired location. First number is longitude, second latitude.')
 @click.option('-l', '--location',
-	type=click.Choice(['Adelaide', 'Arkaroola', 'Barmera']))
+	type=click.Choice(['MtBruce','MtMeharry','Mereenie','Arkaroola',
+						'FowlersGap','Woomera','SidingSpring','Koonamoore',
+						'Moorook','Adelaide']))
+@click.option('--maps/--no-maps', default=False)
 
-def main(path, output_folder, output_file, size_map, size_mean, location, coords):
+def main(path, output_folder, output_file, location, coords, maps):
 
-	df = pd.DataFrame(columns=['date', 'visibility'])
+	df = pd.DataFrame()
 	files = glob.glob(path)
 
-	if location == 'Arkaroola':
-		lat = -30.311667
-		lon = 139.336111
+	if location == 'MtBruce':
+		lat = -22.608
+		lon = 118.144
+	elif location == 'MtMeharry':
+		lat = -22.980
+		lon = 118.588
+	elif location == 'Mereenie':
+		lat = -23.886
+		lon = 132.20
+	elif location == 'Arkaroola':
+		lat = -30.308
+		lon = 139.338
+	elif location == 'FowlersGap':
+		lat = -31.087
+		lon = 141.705
+	elif location == 'Woomera':
+		lat = -31.099
+		lon = 136.786
+	elif location == 'SidingSpring':
+		lat = -31.2755
+		lon = 149.067
+	elif location == 'Koonamoore':
+		lat = -32.064
+		lon = 139.383
+	elif location == 'Moorook':
+		lat = -34.267
+		lon = 140.334
 	elif location == 'Adelaide':
 		lat = -34.928889
 		lon = 138.601111
-	elif location == 'Barmera':
-		lat = -34.25
-		lon = 140.466667
 	else:
 		#if len(location) > 0:
 		#	raise Exception('This location is not pre-defined. Please specify the coordinates.')
@@ -52,102 +72,104 @@ def main(path, output_folder, output_file, size_map, size_mean, location, coords
 			lat = coords[1]
 			location = f'Lon{lon}_Lat{lat}'
 
+
 	for f in files:
 		print(f'Processing file {f}')
 
-		year = int(f[28:32])
-		day = int(f[32:35])
-		hour = int(f[36:38])
-		minute = int(f[38:40])
-		print(year, day, hour, minute)
-		date_time = ( datetime.datetime(year, 1, 1, hour, minute) 
-					+ datetime.timedelta(day - 1) )
-		print(f'Acquisition date: {date_time.strftime("%Y-%m-%d %H:%M")}')
-
 		file = SD(f, SDC.READ)
-		datasets_dic = file.datasets()
+
+		# Seconds since 1993-01-01 at 0:00:00.0 (TAI)
+		sec93 = file.select('Scan_Start_Time').get()[0,0]
+		utc = datetime(1993,1,1,0,0) + timedelta(seconds=sec93)
+		print(f'Acquisition date: {utc.strftime("%Y-%m-%d %H:%M")}')
+
 		lat5km = file.select('Latitude').get()
-		long5km = file.select('Longitude').get()
+		lon5km = file.select('Longitude').get()
 		mask1km = file.select('Cloud_Mask_1km').get()
+		height1km = file.select('cloud_top_height_1km').get()
+		fraction5km = file.select('Cloud_Fraction_Night').get()
 
-		if np.shape(long5km)[0] == 406:
+		tie_rows = np.arange(2,3000,5)[:np.shape(lat5km)[0]]
+		tie_cols = np.arange(2,1500,5)[:np.shape(lat5km)[1]]
+		fine_rows = np.arange(0,3000,1)[:np.shape(mask1km)[0]]
+		fine_cols = np.arange(0,1500,1)[:np.shape(mask1km)[1]]
 
-			tie_rows = np.arange(2,2030,5)
-			tie_cols = np.arange(2,1350,5)
-			fine_rows = np.arange(2030)
-			fine_cols = np.arange(1354)
+		interpolator = GeoInterpolator(
+				(lon5km, lat5km), 
+				(tie_rows, tie_cols), 
+				(fine_rows, fine_cols), 2, 2)
+		lon1km, lat1km = interpolator.interpolate()
 
-			interpolator = GeoInterpolator((long5km, lat5km), 
-											(tie_rows, tie_cols), 
-											(fine_rows, fine_cols), 
-											2, 2)
-			long1km, lat1km = interpolator.interpolate()
+		lon1km = lon1km.flatten()
+		lat1km = lat1km.flatten()
 
-			mask = np.zeros(np.shape(mask1km)[:2])
-			valx = bits_stripping(1,2,mask1km[:,:,0])
+		lon5km = lon5km.flatten()
+		lat5km = lat5km.flatten()
 
-			square_size = 2*size_mean
 
-			lonx = long1km.flatten()
-			latx = lat1km.flatten()
-			val = valx.flatten()
+		cmask_0 = bits_stripping(0, 1, mask1km[:,:,0]).flatten()
+		cmask_1 = bits_stripping(1, 2, mask1km[:,:,0]).flatten()
+		cmask_3 = bits_stripping(3, 1, mask1km[:,:,0]).flatten()
+		cmask_4 = bits_stripping(4, 1, mask1km[:,:,0]).flatten()
+		cmask_5 = bits_stripping(5, 1, mask1km[:,:,0]).flatten()
+		cmask_6 = bits_stripping(6, 2, mask1km[:,:,0]).flatten()
+		cmask_8 = bits_stripping(8, 1, mask1km[:,:,0]).flatten()
+		cmask_14 = bits_stripping(14, 1, mask1km[:,:,0]).flatten()
+		cmask_15 = bits_stripping(15, 1, mask1km[:,:,0]).flatten()
+		cmask_17 = bits_stripping(17, 1, mask1km[:,:,0]).flatten()
+		cmask_18 = bits_stripping(18, 1, mask1km[:,:,0]).flatten()
+		cmask_19 = bits_stripping(19, 1, mask1km[:,:,0]).flatten()
 
-			idx_map = (lonx > (lon - size_map)) & (lonx < (lon+size_map))
-			idx_map = idx_map & (latx > (lat - size_map)) & (latx < (lat + size_map))
 
-			idx_mean = (lonx > (lon - size_mean)) & (lonx < (lon+size_mean))
-			idx_mean = idx_mean & (latx > (lat - size_mean)) & (latx < (lat + size_mean))
+		cheight = height1km.flatten()
+		cfraction = fraction5km.flatten()
 
-			if len(val[idx_mean]) > (size_mean**2 * 25000.):
+		dist1km = np.sqrt( (lon - lon1km) ** 2 + (lat - lat1km) ** 2 )
+		dist5km = np.sqrt( (lon - lon5km) ** 2 + (lat - lat5km) ** 2 )
 
-				mean = np.mean(val[idx_mean])
+		if np.min(dist1km) < 0.005:
+			idx1km = np.argmin(dist1km)
+			idx5km = np.argmin(dist5km)
 
-				grid_x, grid_y = np.mgrid[np.min(lonx[idx_map]):np.max(lonx[idx_map]):100j, np.min(latx[idx_map]):np.max(latx[idx_map]):200j]
-				intp = griddata((lonx[idx_map],latx[idx_map]), val[idx_map], (grid_x,grid_y), method='nearest')
+			df = df.append({
+					'date': utc, 
+					'filename': f,
+					'cmask_0': cmask_0[idx1km],
+					'cmask_1': cmask_1[idx1km],
+					'cmask_3': cmask_3[idx1km],
+					'cmask_4': cmask_4[idx1km],
+					'cmask_5': cmask_5[idx1km],
+					'cmask_6': cmask_6[idx1km],
+					'cmask_8': cmask_8[idx1km],
+					'cmask_14': cmask_14[idx1km],
+					'cmask_15': cmask_15[idx1km],
+					'cmask_17': cmask_17[idx1km],
+					'cmask_18': cmask_18[idx1km],
+					'cmask_19': cmask_19[idx1km],
+					'cloudfraction': cfraction[idx5km],
+					'height':cheight[idx1km],
+					}, 
+					ignore_index=True)
 
-				fig, ax = plt.subplots(figsize=(6,5))
-				img = ax.imshow(intp.T, extent=(
-						  np.min(lonx[idx_map]),
-						  np.max(lonx[idx_map]),
-						  np.max(latx[idx_map]), 
-						  np.min(latx[idx_map])),
-						  cmap='bone',
-						  vmin=0, vmax=3)
-				ax.plot([lon], [lat], marker='x', 
-						markersize=10, mew=3, color="red") #indicates place of interest
-				ax.invert_yaxis()
-				ax.set_ylabel('Latitude / deg')
-				ax.set_xlabel('Longitude / deg')
-				ax.text(0.05, 0.95, f'{location}\n{date_time.strftime("%Y-%m-%d %H:%M")}', 
-					transform = plt.gca().transAxes,
-					horizontalalignment='left', 
-					verticalalignment='top',
-					bbox=dict(facecolor='white', alpha=0.8)) 
-				square = patches.Rectangle(((lon - size_mean), 
-											(lat - size_mean)), 
-											square_size, 
-											square_size, 
-											lw = 1, edgecolor = 'red', 
-											facecolor = 'none')
-				ax.add_patch(square) #creates square over area we are taking the mean of				
-				ax.text(0.95, 0.95, 
-					'Mean Cloud Index: {:.2f}'.format(mean), 
-					transform = plt.gca().transAxes,
-					horizontalalignment='right', 
-					verticalalignment='top',
-					bbox=dict(facecolor='white', alpha=0.8)) 
-				# cloud index of 0 is cloudy, 3 is clear
-				cbar = fig.colorbar(img)
-				cbar.set_label('Cloud Index')
-				fig.savefig(f'{output_folder}{location}_{date_time.date()}_{date_time.time()}.pdf')
+			if maps:
 
-				df = df.append({'date': date_time, 'visibility':mean}, ignore_index=True)
-
-			else:
-				print('Map contains not enough information')
-
+				size_map = 0.25
+				idx_map = (lon1km > (lon - size_map)) & (lon1km < (lon + size_map))
+				idx_map = idx_map & (lat1km > (lat - size_map)) & (lat1km < (lat + size_map))
+				
+				grid_x, grid_y = np.mgrid[np.min(lon1km[idx_map]):np.max(lon1km[idx_map]):100j, 
+										  np.min(lat1km[idx_map]):np.max(lat1km[idx_map]):200j]
+				intp_cmask = griddata((lon1km[idx_map],lat1km[idx_map]), cmask_1[idx_map], 
+											(grid_x,grid_y), method='nearest')
+				intp_cheight = griddata((lon1km[idx_map],lat1km[idx_map]), cheight[idx_map], 
+											(grid_x,grid_y), method='nearest')
+				
+				np.save(f'{output_folder}{location}_cloudmask_{utc.date()}_{utc.time()}_{np.min(lon1km[idx_map])}_{np.max(lon1km[idx_map])}_{np.min(lat1km[idx_map])}_{np.max(lat1km[idx_map])}.npy', intp_cmask)
+				np.save(f'{output_folder}{location}_cloudheight_{utc.date()}_{utc.time()}_{np.min(lon1km[idx_map])}_{np.max(lon1km[idx_map])}_{np.min(lat1km[idx_map])}_{np.max(lat1km[idx_map])}.npy', intp_cheight)
+		
 		else:
-			print('Strange shape')
+			print('Site not on map')
+
 	
 	df = df.sort_values(by='date', ascending=True) #reorders dataframe so histogram will be in correct order
 	if output_file  == None:
@@ -158,8 +180,8 @@ def main(path, output_folder, output_file, size_map, size_mean, location, coords
 
 
 def bits_stripping(bit_start,bit_count,value):
-	bitmask=pow(2,bit_start+bit_count)-1
-	return np.right_shift(np.bitwise_and(value,bitmask),bit_start)
+	bitmask = pow(2,bit_start+bit_count)-1
+	return np.right_shift(np.bitwise_and(value,bitmask), bit_start)
 
 
 if __name__ == '__main__':
